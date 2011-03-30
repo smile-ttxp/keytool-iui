9f7472d422c3461424639155e5e1b75ef781baa5
bantchao@gmail.com <bantchao@gmail.com@8246f0f5-e154-6a54-4521-29ac8b02ce23>
2011-03-30 21:47:42 +0000

    JavaHelp
    git-svn-id: http://keytool-iui.googlecode.com/svn/trunk@23 8246f0f5-e154-6a54-4521-29ac8b02ce23

---
+++ libs/internal/helpMain/rc15ktl_hs/helpset.hs
@@ -0,0 +1,19 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+"http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+<helpset version="1.0">
+ <title>KeyTool IUI - User's Guide</title>
+ <maps>
+  <homeID>_welcome_</homeID>
+  <mapref location="map.jhm"/>
+ </maps>
+
+ <view>
+  <name>toc name</name>
+  <label>T.O.C.</label>
+  <type>javax.help.TOCView</type>
+  <data>toc.xml</data>
+ </view>
+
+</helpset>
